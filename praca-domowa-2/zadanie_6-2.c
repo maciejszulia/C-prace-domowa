f10288e99434e07569e9f79f1b3043663dfa7c0c
Maciej Szulia <maciej.szulia@gmail.com>
2021-03-10 16:34:42 +0100

    praca domowa 2

---
+++ praca-domowa-2/zadanie_6-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	//rozdział 6 zad 2
+
+	for (int j = 1; j <= 5; j++)
+	{
+		for (int i = 1; i <= j; i++)
+		{
+			printf("\$");
+			
+		}
+		printf("\n");
+	}
+
+
+	
+	
+	return 0;
+}
+
+//Maciej Szulia 154733
