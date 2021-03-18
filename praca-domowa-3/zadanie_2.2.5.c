3a1284c3697791a3a9d1419620263b78ba506b8d
Maciej Szulia <maciej.szulia@gmail.com>
2021-03-18 10:47:02 +0100

    zadania

---
+++ praca-domowa-3/zadanie_2.2.5.c
@@ -0,0 +1,16 @@
+//Krzaczkowski, Zadanie 2.2.5
+#include <stdio.h>
+#include <math.h>
+
+int potega(int liczba)
+{
+	return pow(2, liczba);
+}
+
+int main()
+{
+	printf("%d", potega(3));
+
+
+	return 0;
+}
---
+++ praca-domowa-3/zadanie_2.2.6.c
@@ -0,0 +1,23 @@
+//Krzaczkowski, Zadanie 2.2.6
+#include <stdio.h>
+#include <math.h>
+
+long long int funkcja(unsigned int n, unsigned int m)
+{
+	if (n == 0 && m == 0)
+		return 0;
+
+	return pow(n, m);
+}
+
+int main()
+{
+
+	printf("%lld ", funkcja(0, 0));
+
+	printf("%lld", funkcja(10, 10));
+
+
+
+	return 0;
+}
