cf437f349f82964c8d39d24165d8d7dc1bfe79f8
Maciej Szulia <maciej.szulia@gmail.com>
2021-03-25 12:04:27 +0100

    pr dom 4

---
+++ praca-domowa-4/zadanie_3.2.5 .c
@@ -0,0 +1,2 @@
+// 3.2.5 Napisz funkcję, której argumentami są dwa wskaźniki do stałych typu int
+// zaś zwracaną wartością jest suma wartości zmiennych wskazywanych przez argumenty.
---
+++ praca-domowa-4/zadanie_3.2.6.c
@@ -0,0 +1,2 @@
+// 3.2.6 Napisz funkcję, której argumentami są n typu int oraz w wskaźnik do int
+// która przepisuje wartośćndo zmiennej wskazywanej przezw.
