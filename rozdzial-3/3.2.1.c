bd46607424d5892d44ccf111ed80eb5dab0af75d
maciejszulia <maciej.szulia@gmail.com>
2021-06-13 14:54:34 +0200

    krzaczkowski to co robilem sam

---
+++ rozdzial-3/3.2.1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int funkcja(int *a, int *b)
+{
+	if (*a > *b)
+		return *b;
+	else
+		return *a;
+}
+
+int main()
+{
+	int a = 1;
+	int b = 2;
+	printf("mniejsza jest: %i", funkcja(&a, &b));
+
+
+
+	return 0;
+}
